7c00e918d02570ef57b6f418fe6523f0eed66899
Samilly <samillynunes19@gmail.com>
2024-05-17 10:39:46 -0300

    renderization: rendering static and ssr

---
+++ src/pages/static1.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function static1(){
+
+    // const [num, setNum] = useState(0);
+
+    // // Ao usar essa estrategia abaixo, deixa de ser um conteudo estatico e passa a ser um SSR, pois precisa ser 
+    // // re-renderizado ou nao 
+    // useEffect(()=>{
+    //     setNum(Math.random());
+    // }, []);
+
+    return (
+        <div>
+            <h1> Estático #01 </h1>
+            {/* <span> {num} </span> */}
+        </div>
+    );
+}
